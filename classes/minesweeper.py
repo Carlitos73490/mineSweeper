df309ad078dfa012cf5b33683645f412f88f463d
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2022-09-13 17:14:43 +0200

    WIP

--- classes/minesweeper.py
+++ classes/minesweeper.py
@@ -1,7 +1,36 @@
+from enum import Enum
+
+
+class action_title(Enum):
+    OPEN = "Ouvrir la case"
+    FLAG = "Flagger la case"
+
+
 class Minesweeper:
-    grid_dimension = 0
+    grid_dimension = None
+    action_splits = None
+    is_playing = False
 
-    def __init__(self,grid_dimension):
+
+    def new_game(self,grid_dimension):
+        self.is_playing = True
         self.grid_dimension = grid_dimension
+        while self.is_playing:
+            print("Début de la sélection")
+            self.splits = input("{action} x y : ").split()
+            self.actions()
+    def quit(self):
+        self.is_playing = False
+    def actions(self):
+        if self.action_split[0] == "F":
+            self.flag()
+        elif self.action_split[0] == "Q":
+            self.quit()
+        else:
+            self.open()
 
+    def open(self):
+        print(action_title.FLAG.value, " ", self.action_split[1], ",", self.action_split[2])
 
+    def flag(self):
+        print(action_title.OPEN.value, " ", self.action_split[0], ",", self.action_split[1])
--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import sys
 from classes.minesweeper import Minesweeper
 
 if __name__ == '__main__':
-    m = Minesweeper(sys.argv[1])
-    print(m.grid_dimension)
+    m = Minesweeper()
+    m.new_game(sys.argv[1])
